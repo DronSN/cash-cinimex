51e01ebbd3c9b89bfb2b9b6bc7b5cced9c905ba3
Andrey Skvortsov <skvorez@inbox.ru>
2020-01-14 22:47:28 +0300

    Add empty test file for Home desktop synchronization

---
+++ src/test/java/ru/skvrez/cash_cinimex/CashTests.java
@@ -0,0 +1,14 @@
+package ru.skvrez.cash_cinimex;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CashTests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
